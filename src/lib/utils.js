bd33665d770758880b11da50019b71ef3d5f4c66
bayre <ben@ayre.co.uk>
2022-05-20 11:49:19 +0100

    fix frontmatter regression

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,7 +2,7 @@ import yaml from 'js-yaml'
 
 export function addFrontmatter(ast, config) {
   const frontmatter = ast.attributes.frontmatter ? yaml.load(ast.attributes.frontmatter) : {}
-  const markdoc = Object.assign(config?.variables?.markdoc || {}, { frontmatter })
-  const variables = Object.assign(config?.variables || {}, { markdoc })
-	return Object.assign(config, { variables })
+  const markdoc = { ...(config?.variables?.markdoc || {}), frontmatter }
+  const variables = { ...(config?.variables || {}), markdoc }
+  return {...config, variables };
 }
